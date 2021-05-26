fb267438676ca2e7ba331760889e2783db69561c
Gareth Breeze <you@example.com>
2021-05-26 18:21:07 +0100

    Got my home page i.e. list view working

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 
 
 class Image(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=32)
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post, Image
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = 'blog/home.html'
+    context_object_name = 'post_list'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-last_modified')
+
